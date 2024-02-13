8b264092d4e2ff7f6d669ccb79d9c8821cd1366d
isshosan <rasyidridhom@gmail.com>
2024-02-14 00:22:36 +0700

    gantiwarna

--- latihan1.js
+++ latihan1.js
@@ -1,19 +1,30 @@
 document.title = 'rasyid ridho'
+const body = document.body
 const btn1 = document.getElementById('btn1')
 const btn2 = document.querySelector('#btn2')
 
-console.log(btn1)
-console.log(btn2)
+const defaultText = "test"
+btn1.textContent = defaultText
 
 btn1.style.border = 'none' //adjust border, none is remove all border
 btn1.style.padding = '16px' //resize background button
 btn1.style.fontSize = '24px'
 btn1.style.background = 'tomato' //background color
 
+
+function clickButton () {
+    btn1.style.background = "aqua"
+    const newText = document.createElement('p')
+    newText.textContent = "hai apa kabar?"
+    body.append(newText)
+}
+
 function gantiWarna2() {
-    console.log("aman");
-    alert("aman") ;//popup aler ketika di klik
     btn2.style.background = 'aqua';
+}
+
+function gantiWarnaLagi () {
+    btn2.style.background = 'red'
 }
 
 function ubahText () {
@@ -22,7 +33,7 @@ function ubahText () {
 }
 
 function oriText () {
-    btn1.textContent = "klik tombol ini 1"
+    btn1.textContent = defaultText
 
 }
 //isi gantiWarna pada html merupakan function dari gantiWarna pada JS 
